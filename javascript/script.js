062fdac10cdd16956de23c4af41c60d6b184924c
letyresina <leticiaresina@gmail.com>
2023-05-19 19:06:23 -0300

    Feito o scrolldown

--- javascript/script.js
+++ javascript/script.js
@@ -5,3 +5,10 @@ function alertaBotaoCadastro(){
 function alertaBotaoLogin(){
     alert("Enviamos um e-mail para redefinir sua senha")
 }
+
+const paraScrollar = document.getElementById('scroll');
+const secaoAlvo = document.getElementById('conteudoHome');
+
+paraScrollar.addEventListener('click', function() {
+    secaoAlvo.scrollIntoView({ behavior: 'smooth', duration:1.8});
+});
