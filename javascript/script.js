3127aa702aa01e2c126118289eaabc83af941a2d
letyresina <leticiaresina@gmail.com>
2023-05-19 20:58:22 -0300

    feito validação em javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,3 @@
-function alertaBotaoCadastro(){
-    alert("Seja bem vindo(a)!")
-}
-
 function alertaBotaoLogin(){
     alert("Enviamos um e-mail para redefinir sua senha")
 }
@@ -12,3 +8,15 @@ const secaoAlvo = document.getElementById('conteudoHome');
 paraScrollar.addEventListener('click', function() {
     secaoAlvo.scrollIntoView({ behavior: 'smooth', duration:1.8});
 });
+
+function validacaoLogin(){
+
+    let emailUsuarioLogin = document.getElementById('emailUsuarioLogin').value
+    let senhaUsuarioLogin = document.getElementById('senhaUsuarioLogin').value
+
+    if (emailUsuarioLogin == "" || senhaUsuarioLogin == ""){
+        alert('Preencha todos campos antes de prosseguir')
+    } else {
+        alert("Seja bem vindo(a)!")
+    }
+}
